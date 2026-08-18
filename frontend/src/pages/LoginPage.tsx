35682847468170ff34cf31ab9f64088d88d2cfb7
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-08-18 22:50:05 +0200

    feat: create register and login page components

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../features/auth/LoginForm";
+
+export function LoginPage() {
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <LoginForm />
+    </main>
+  );
+}
---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "../features/auth/RegisterForm";
+
+export function RegisterPage() {
+  return (
+    <main>
+      <h1>Create your account</h1>
+
+      <RegisterForm />
+    </main>
+  );
+}
