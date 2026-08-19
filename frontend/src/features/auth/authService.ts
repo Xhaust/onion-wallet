973aa41dbee9bf0c65d814ac9b30d9845c2b1bfe
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-08-19 18:05:23 +0200

    feat: create api services to get user data and logout

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -1,5 +1,23 @@
 import { apiFetch } from "../../services/api";
 
+export interface UserData {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export async function getUserData(): Promise<UserData> {
+  const response = await apiFetch("/users/me/", {
+    method: "GET"
+  });
+  
+  if (!response.ok) {
+    throw new Error("Couldn't retrieve data");
+  }
+
+  return response.json();
+}
+
 interface RegisterData {
   username: string;
   email: string;
@@ -36,4 +54,20 @@ export async function login(data: LoginData) {
   }
 
   return response.json();
+}
+
+export async function logout() {
+  const csrfToken = document.cookie.match(/csrftoken=([^;]+)/)?.[1] ?? '';
+  const response = await apiFetch("/users/logout/", {
+    method: "POST",
+    headers: {
+      'X-CSRFToken': csrfToken,
+    }
+  })
+
+  if (!response.ok) {
+    throw new Error("Logout failed");
+  }
+
+  localStorage.removeItem('csrfToken');
 }
